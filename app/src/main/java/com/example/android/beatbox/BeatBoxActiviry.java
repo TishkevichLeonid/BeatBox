09c96de74f72338eaa5a0a772f00568512b32a77
TishkevichLeonid <leonidtishkevich@gmail.com>
2017-07-09 13:06:34 +0300

    init

---
+++ app/src/main/java/com/example/android/beatbox/BeatBoxActiviry.java
@@ -0,0 +1,13 @@
+package com.example.android.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BeatBoxActiviry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_beat_box_activiry);
+    }
+}
